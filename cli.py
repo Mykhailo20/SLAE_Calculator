c7b21067ab7a9f383be2cbcd7a48ef84be0535c3
Mykhailo <myhailo20189@gmail.com>
2023-01-29 22:45:29 +0200

    Input from text files

--- cli.py
+++ cli.py
@@ -12,14 +12,34 @@ if("1" in choice):
     rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
 
     matrix = [[0 for j in range(cols)] for i in range(rows)]
-    """for i in range(rows):
-        for j in range(cols):
-            matrix[i][j] = float(input(f"Enter the {j+1} coeficient of {i+1} row: "))
-    """
+
     for i in range(rows):
-        row_coeficients = input(f"Enter the coeficients of {i+1} equation: ").split(" ")
+        row_coefficients = input(f"Enter the coefficients of {i+1} equation: ").split(" ")
         for j in range(cols):
-            matrix[i][j] = float(row_coeficients[j])
+            matrix[i][j] = float(row_coefficients[j])
+
+else:
+    filepath = input("Enter the file name: ")
+    try:
+        with open(filepath, "r") as file:
+            input_list = file.readlines()
+    except FileNotFoundError:
+        file_error = """
+The file with this name/path does not exist.
+Please check if the file exists and restart the program.
+"""
+        print(file_error)
+        exit()
+    dimension_list = input_list[0].split(" ")
+    rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
+
+    matrix = [[0 for j in range(cols)] for i in range(rows)]
+
+    # Reading coefficients
+    for i in range(1, (rows+1)):
+        row_coefficients = input_list[i].split(" ")
+        for j in range(cols):
+            matrix[(i-1)][j] = float(row_coefficients[j])
 
 """matrix = [[1.26, -2.34, 1.17, 3.14],
           [0.75, 1.24, -0.48, -1.17],
@@ -37,3 +57,4 @@ for root in roots_list:
     print("%.5f"%(root), end=" ")
 
 print('\n')
+
