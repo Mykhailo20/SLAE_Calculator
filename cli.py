4219c52beddc0d234d053db7b56058b871f457c3
Mykhailo <myhailo20189@gmail.com>
2023-02-03 21:57:21 +0200

    CLI: Handle invalid input for matrix dimension and coefficients (letters instead of numbers)

--- cli.py
+++ cli.py
@@ -5,6 +5,14 @@ choice_text = """Please, enter the method of entering data:
 1 - keyboard input;
 2 - read from a file.
 Your choice: """
+
+invalid_matrix_dimension = """Invalid input!
+You can enter only natural integers for matrix dimension. """
+
+invalid_matrix_coefficients = """Invalid input!
+You can enter only numbers (integers or floats) for matrix coefficients.
+Floats must be divided by dot(.). For example: 3.15."""
+
 method_choice = """Please, enter the method of solving the SLAE:
 1 - Gauss method;
 2 - LU method.
@@ -13,18 +21,29 @@ Your choice: """
 choice = input(choice_text)
 
 if("1" in choice):
-    dimension_list = input("Enter the dimension of matrix: ").split(" ")
-    rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
+
+    try:
+        dimension_list = input("Enter the dimension of matrix: ").split(" ")
+        rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
+    except ValueError:
+        print(invalid_matrix_dimension)
+        print("Please, restart the program.")
+        exit()
 
     matrix = [[0 for j in range(cols)] for i in range(rows)]
 
     for i in range(rows):
-        row_coefficients = input(f"Enter the coefficients of {i+1} equation: ").split(" ")
-        for j in range(cols):
-            matrix[i][j] = float(row_coefficients[j])
+        try:
+            row_coefficients = input(f"Enter the coefficients of {i+1} equation: ").split(" ")
+            for j in range(cols):
+                matrix[i][j] = float(row_coefficients[j])
+        except ValueError:
+            print(invalid_matrix_coefficients)
+            print("Please, restart the program.")
+            exit()
 
 else:
-    filepath = input("Enter the file name: ")
+    filepath = input("Enter the file path: ")
     try:
         with open(filepath, "r") as file:
             input_list = file.readlines()
@@ -35,16 +54,26 @@ Please check if the file exists and restart the program.
 """
         print(file_error)
         exit()
-    dimension_list = input_list[0].split(" ")
-    rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
+
+    try:
+        dimension_list = input_list[0].split(" ")
+        rows, cols = [int(dimension_list[0]), int(dimension_list[1])]
+    except ValueError:
+        print(invalid_matrix_dimension)
+        print("Check if the file data is correct and restart the program.")
+        exit()
 
     matrix = [[0 for j in range(cols)] for i in range(rows)]
-
-    # Reading coefficients
-    for i in range(1, (rows+1)):
-        row_coefficients = input_list[i].split(" ")
-        for j in range(cols):
-            matrix[(i-1)][j] = float(row_coefficients[j])
+    try:
+        # Reading coefficients
+        for i in range(1, (rows+1)):
+            row_coefficients = input_list[i].split(" ")
+            for j in range(cols):
+                matrix[(i-1)][j] = float(row_coefficients[j])
+    except ValueError:
+        print(invalid_matrix_coefficients)
+        print("Check if the file data is correct and restart the program.")
+        exit()
 
 print("Initial matrix: ")
 exact_methods.print_matrix(matrix, 3, 4)
@@ -67,7 +96,7 @@ else:
 
 print("\nRoots are: ", end=" ")
 for root in roots_list:
-    print("%.5f"%(root), end=" ")
+    print("%.5f" % root, end=" ")
 
 print('\n')
 
